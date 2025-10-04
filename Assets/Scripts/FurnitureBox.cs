5d9bb3586eea3ae51c95ddbe71e80951c6057767
Branden Soropia <BrandenSoropia@users.noreply.github.com>
2025-10-04 18:48:07 -0400

    InventoryFix (#12)
    Ensure the inventory and spawning feature works fine with main
    
    Co-authored-by: LoganZhangX <logan_zhang_x@outlook.com>

---
+++ Assets/Scripts/FurnitureBox.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class FurnitureBox : MonoBehaviour
+{
+    public List<FurniturePiece> items;
+    public InventoryManager inventoryManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
