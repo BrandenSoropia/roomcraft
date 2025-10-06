9c05c942378179a6af0664bf16f36cb72e8e1777
Branden Soropia <BrandenSoropia@users.noreply.github.com>
2025-10-06 18:35:15 -0400

    Added UI and attached it to placement state updating. Only allow placement state to decrement once when furniture is not fully in area. Added docs.

--- Assets/Scripts/GameState/AreaTrigger.cs
+++ Assets/Scripts/GameState/AreaTrigger.cs
@@ -1,13 +1,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+Place this on each PlacementArea.
+1 PlacementArea to 1 furniture.
+
+Requirements:
+- Each furniture piece must have:
+  - a collider
+  - either tagged as "Furniture" or "Marker" since we have to count all colliders present
+*/
 public class AreaTrigger : MonoBehaviour
 {
     [SerializeField] GameManager gameManager;
     [SerializeField] Transform parentObject; // the root object whose children we care about
 
-
     private HashSet<Collider> inside = new HashSet<Collider>();
+    private bool _hasReducedPlacementCall = false;
 
 
     private Collider[] childColliders;
@@ -27,6 +36,7 @@ public class AreaTrigger : MonoBehaviour
             if (inside.Count == childColliders.Length)
             {
                 gameManager.IncrementNumCorrectPlacementFurniture();
+                _hasReducedPlacementCall = false; // Reset this so we can reduce the total placed counter again
             }
         }
     }
@@ -37,9 +47,14 @@ public class AreaTrigger : MonoBehaviour
         {
             inside.Remove(other);
 
-            if (inside.Count == 0)
+            /*
+            Only allow reducing the total placement counter once per exit.
+            Without this, it will decrement the progress each time any piece exits the area.
+            */
+            if (!_hasReducedPlacementCall)
             {
                 gameManager.DecrementNumCorrectPlacementFurniture();
+                _hasReducedPlacementCall = true;
             }
 
         }
--- Assets/Scripts/GameState/GameManager.cs
+++ Assets/Scripts/GameState/GameManager.cs
@@ -1,16 +1,21 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [Header("Game Stats")]
-    public int _numCorrectPlacementFurniture = 0;
+    [SerializeField] int _numCorrectPlacementFurniture = 0;
+    [SerializeField] int _numTotalFurnitureToBuild = 0;
+
+    [Header("Controlled UI")]
+    [SerializeField] TextMeshProUGUI placementProgressText;
 
     private static GameManager _instance;
 
     public static GameManager Instance { get { return _instance; } }
 
     // Maintain singleton
-    private void Awake()
+    void Awake()
     {
         if (_instance != null && _instance != this)
         {
@@ -22,13 +27,25 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        UpdatePlacementProgressGUI(); // Make placement UI is properly matching text to level configuration in GameManager 
+    }
+
     public void IncrementNumCorrectPlacementFurniture()
     {
         _numCorrectPlacementFurniture += 1;
+        UpdatePlacementProgressGUI();
     }
 
     public void DecrementNumCorrectPlacementFurniture()
     {
         _numCorrectPlacementFurniture -= 1;
+        UpdatePlacementProgressGUI();
+    }
+
+    void UpdatePlacementProgressGUI()
+    {
+        placementProgressText.text = $"Furniture Placed: {_numCorrectPlacementFurniture}/{_numTotalFurnitureToBuild}";
     }
 }
