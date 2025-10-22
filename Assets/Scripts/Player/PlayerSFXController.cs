f8301e004917299e904cbf59c4e35ab8bc479cbb
Branden Soropia <BrandenSoropia@users.noreply.github.com>
2025-10-21 20:49:05 -0400

    Added new sfx for toggle mode, open box and spawn piece.

--- Assets/Scripts/Player/PlayerSFXController.cs
+++ Assets/Scripts/Player/PlayerSFXController.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class PlayerSFXController : MonoBehaviour
 {
-    private AudioSource myAudioSource;
 
     [Header("Build SFX")]
 
@@ -10,16 +9,27 @@ public class PlayerSFXController : MonoBehaviour
     [SerializeField] AudioClip rotateSfx;
     [SerializeField] AudioClip selectPieceSfx;
     [SerializeField] AudioClip deselectPieceSfx;
+    [SerializeField] AudioClip unboxSfx;
 
     [Header("Inventory SFX")]
     [SerializeField] AudioClip closeInventorySfx;
     [SerializeField] AudioClip openInventorySfx;
+    [SerializeField] AudioClip spawnPieceSfx;
+
+    [Header("Toggle Game Mode SFX")]
+    [SerializeField, Range(0f, 1f)] float gameModeVolume;
+
+    [SerializeField] AudioClip toBuildModeSfx;
+    [SerializeField] AudioClip toIsometricModeSfx;
+
+    private AudioSource myAudioSource;
 
     void Start()
     {
         myAudioSource = GetComponent<AudioSource>();
     }
 
+    // Build SFX
     public void PlayRotateSFX()
     {
         myAudioSource.PlayOneShot(rotateSfx);
@@ -40,6 +50,13 @@ public class PlayerSFXController : MonoBehaviour
         myAudioSource.PlayOneShot(deselectPieceSfx);
     }
 
+    public void PlayUnboxSFX()
+    {
+        myAudioSource.PlayOneShot(unboxSfx);
+    }
+
+    // Inventory SFX
+
     public void PlayCloseInventorySFX()
     {
         myAudioSource.PlayOneShot(closeInventorySfx);
@@ -48,5 +65,24 @@ public class PlayerSFXController : MonoBehaviour
     public void PlayOpenInventorySFX()
     {
         myAudioSource.PlayOneShot(openInventorySfx);
+    }
+
+    public void PlaySpawnPieceSFX()
+    {
+        myAudioSource.PlayOneShot(spawnPieceSfx);
+    }
+
+    /*
+    Game Mode SFX
+    These are a little loud so just quiet them down programatically.
+    */
+    public void PlayToBuildModeSFX()
+    {
+        myAudioSource.PlayOneShot(toBuildModeSfx, gameModeVolume);
+    }
+
+    public void PlayToIsometricModeSFX()
+    {
+        myAudioSource.PlayOneShot(toIsometricModeSfx, gameModeVolume);
     }
 }
