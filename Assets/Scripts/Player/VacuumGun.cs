f9f59aadceab5a9ae6d5f86924ffb14822d77ac0
Enrui Lin <linedison3@gmail.com>
2025-10-30 23:16:03 -0400

    Refactor VacuumGun input handling to unify suck and shoot actions into a single vacuum action

--- Assets/Scripts/Player/VacuumGun.cs
+++ Assets/Scripts/Player/VacuumGun.cs
@@ -3,9 +3,8 @@ using UnityEngine.InputSystem;
 
 public class VacuumGun : MonoBehaviour
 {
-    [Header("Input (drag from .inputactions)")]
-    [SerializeField] private InputActionReference suck;   // Player/Suck
-    [SerializeField] private InputActionReference shoot;  // Player/Shoot
+    [Header("Input")]
+    [SerializeField] private InputActionReference vacuum; // Player/Vacuum
 
     [Header("Raycast & Firing")]
     [SerializeField] private Camera playerCamera;
@@ -14,73 +13,85 @@ public class VacuumGun : MonoBehaviour
     [SerializeField] private float shootForce = 20f;
 
     [Header("Targeting")]
-    [SerializeField] private string suckableTag = "Suckable"; // tag your pieces
+    [SerializeField] private string suckableTag = "Suckable";
 
-    private GameObject storedPrefab; // what we "sucked" (simple version)
+    // this stores the thing we grabbed
+    private GameObject storedPrefab;
 
     private void OnEnable()
     {
-        if (suck != null)
+        if (vacuum != null)
         {
-            suck.action.performed += OnSuck;
-            suck.action.Enable();
-        }
-        if (shoot != null)
-        {
-            shoot.action.performed += OnShoot;
-            shoot.action.Enable();
+            vacuum.action.performed += OnVacuumPressed;
+            vacuum.action.Enable();
         }
     }
 
     private void OnDisable()
     {
-        if (suck != null)
+        if (vacuum != null)
         {
-            suck.action.performed -= OnSuck;
-            suck.action.Disable();
-        }
-        if (shoot != null)
-        {
-            shoot.action.performed -= OnShoot;
-            shoot.action.Disable();
+            vacuum.action.performed -= OnVacuumPressed;
+            vacuum.action.Disable();
         }
     }
 
-    private void OnSuck(InputAction.CallbackContext _)
+    private void OnVacuumPressed(InputAction.CallbackContext _)
     {
-        // add console message
-        Debug.Log("Attempting to suck...");
-        if (storedPrefab != null) return; // already have one stored
+        // if we don't have anything yet -> try to suck
+        if (storedPrefab == null)
+        {
+            TrySuck();
+        }
+        else
+        {
+            // we already have something -> shoot it
+            TryShoot();
+        }
+    }
 
+    private void TrySuck()
+    {
+        // raycast from center of screen
         Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
         if (Physics.Raycast(ray, out RaycastHit hit, suckRange))
         {
-            var go = hit.collider.gameObject;
-            if (go.CompareTag(suckableTag))
+            GameObject target = hit.collider.gameObject;
+
+            // only grab valid targets
+            if (target.CompareTag(suckableTag))
             {
-                // simplest: "store" it and hide the original
-                storedPrefab = go;
-                go.SetActive(false);
-                // add console message
-                Debug.Log("Sucked: " + go.name);
+                storedPrefab = target;
+
+                // hide the original object so it looks "captured"
+                storedPrefab.SetActive(false);
+
+                // optional: you could play a suck FX here
             }
         }
     }
 
-    private void OnShoot(InputAction.CallbackContext _)
+    private void TryShoot()
     {
+        // safety check
         if (storedPrefab == null) return;
 
-        // Recreate/launch it from the muzzle
-        GameObject spawned = Instantiate(storedPrefab, muzzleOrHoldPoint.position, muzzleOrHoldPoint.rotation);
+        // spawn a copy at the muzzle / hold point
+        GameObject spawned =
+            Instantiate(storedPrefab, muzzleOrHoldPoint.position, muzzleOrHoldPoint.rotation);
+
         spawned.SetActive(true);
 
-        if (spawned.TryGetComponent<Rigidbody>(out var rb))
-            rb.linearVelocity = muzzleOrHoldPoint.forward * shootForce;
+        if (spawned.TryGetComponent<Rigidbody>(out Rigidbody rb))
+        {
+            rb.velocity = muzzleOrHoldPoint.forward * shootForce;
+        }
 
-            // add console message
-        Debug.Log("Shot: " + spawned.name);
-
+        // we "used up" the stored item
         storedPrefab = null;
+
+        // optional: you could also Destroy() the original hidden object now,
+        // or leave it inactive forever, depending on your design:
+        // Destroy(storedPrefab); storedPrefab = null;
     }
 }
