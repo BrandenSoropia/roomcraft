031d43b16e478f50a3b2c6b8ed366209743b8a03
Branden Soropia <BrandenSoropia@users.noreply.github.com>
2025-11-21 12:17:12 -0500

    Support controller input on pause screen and Start screen.

--- Assets/Scripts/UI/MenuSelector.cs
+++ Assets/Scripts/UI/MenuSelector.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
+using Unity.VisualScripting;
+using System;
 
 public class MenuSelector : MonoBehaviour
 {
@@ -12,6 +16,15 @@ public class MenuSelector : MonoBehaviour
 
     private int currentIndex = 0;
     private RectTransform targetRect;
+
+    // Needed to listen to controller events
+    InputSystemUIInputModule _uiModule;
+    // References to remove listeners
+    InputAction _moveAction;
+    bool upTriggered = false;
+    bool downTriggered = false;
+    [SerializeField, Range(0, 1)] float moveSensitivity = 0.9f;
+    InputAction _submitAction;
 
     void Start()
     {
@@ -33,6 +46,52 @@ public class MenuSelector : MonoBehaviour
         }
 
         EventSystem.current.SetSelectedGameObject(buttons[currentIndex].gameObject);
+    }
+
+    void OnEnable()
+    {
+        /*
+        Input System UI Input Module doesn't work like Player Input's event handler with its "On..." handlers.
+
+        We have to manually subscribe to UI Input's events like below. Each control will have a matching name
+        from the ones seen in the "Input System UI Input Module" component on the EventSystem.
+        */
+        _uiModule = EventSystem.current?.GetComponent<UnityEngine.InputSystem.UI.InputSystemUIInputModule>();
+
+        if (_uiModule == null)
+        {
+            Debug.LogWarning("No InputSystemUIInputModule found on current EventSystem.");
+            return;
+        }
+
+        if (_uiModule != null)
+        {
+            // Note: can't use ".started" since UI / Move is config to pass-through while ".started" is only for buttons
+            if (_uiModule.move != null)
+            {
+                Debug.Log("MenuSelector: onMove attached");
+                _moveAction = _uiModule.move;
+                _moveAction.performed += OnMovePerformed;
+                _moveAction.canceled += OnMoveCanceled;
+            }
+
+            if (_uiModule.submit != null)
+            {
+                Debug.Log("MenuSelector: onSubmit attached");
+                _submitAction = _uiModule.submit;
+                _submitAction.performed += OnSubmitPerformed;
+            }
+
+        }
+    }
+
+    void OnDisable()
+    {
+        if (_moveAction != null)
+            _moveAction.performed -= OnMovePerformed;
+
+        if (_submitAction != null)
+            _submitAction.performed -= OnSubmitPerformed;
     }
 
     void Update()
@@ -74,5 +133,45 @@ public class MenuSelector : MonoBehaviour
         currentIndex = newIndex;
         targetRect = buttons[currentIndex].GetComponent<RectTransform>();
         EventSystem.current.SetSelectedGameObject(buttons[currentIndex].gameObject);
+    }
+
+    // CONTROLLER SUPPORT
+    void OnSubmitPerformed(InputAction.CallbackContext context)
+    {
+        buttons[currentIndex].onClick.Invoke();
+    }
+
+    // Uses up/down triggered flags to make this handle the movement "once" until the move stick returns to 0
+    void OnMovePerformed(InputAction.CallbackContext ctx)
+    {
+        Vector2 value = ctx.ReadValue<Vector2>();
+        Debug.Log("### called");
+
+        if (value.y > moveSensitivity && !upTriggered)
+        {
+            upTriggered = true;
+            downTriggered = false;
+            Debug.Log("### MenuSelector: moved up");
+            MoveSelection(-1);
+        }
+        else if (value.y < -moveSensitivity && !downTriggered)
+        {
+            Debug.Log("### MenuSelector: moved down");
+            upTriggered = false;
+            downTriggered = true;
+            MoveSelection(1);
+        }
+        else
+        {
+            // Dead zone
+            upTriggered = false;
+            downTriggered = false;
+        }
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext ctx)
+    {
+        upTriggered = false;
+        downTriggered = false;
     }
 }
