2f28017df0a650abf84e08a69e5a80a27b9faee2
Branden Soropia <BrandenSoropia@users.noreply.github.com>
2025-11-22 20:39:34 -0500

    Controller screen added and controlled.

--- Assets/Scripts/UI/Pause/SettingsUIController.cs
+++ Assets/Scripts/UI/Pause/SettingsUIController.cs
@@ -1,13 +1,54 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
 
 public class SettingsUIController : MonoBehaviour
 {
     [SerializeField] GameObject myContent;
     PauseManager pauseManager;
 
+    InputSystemUIInputModule _uiModule;
+    InputAction _cancel;
+
     void Start()
     {
         pauseManager = FindFirstObjectByType<PauseManager>();
+    }
+
+    void OnEnable()
+    {
+        _uiModule = EventSystem.current.GetComponent<InputSystemUIInputModule>();
+        if (_uiModule == null)
+        {
+            Debug.LogWarning("SettingsUIController: No InputSystemUIInputModule found on current EventSystem.");
+            return;
+        }
+
+        if (_uiModule != null)
+        {
+            if (_uiModule.cancel != null)
+            {
+                Debug.Log("SettingsUIController: cancel attached");
+                _cancel = _uiModule.cancel;
+                _cancel.performed += OnCancelPerformed;
+            }
+        }
+    }
+
+    private void OnCancelPerformed(InputAction.CallbackContext context)
+    {
+        Debug.Log("SettingsUIController: back called");
+        Back();
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("SettingsUIController: cancel deattached");
+
+
+        if (_cancel != null)
+            _cancel.performed -= OnCancelPerformed;
     }
 
     public void Open()
@@ -41,6 +82,8 @@ public class SettingsUIController : MonoBehaviour
 
     public void Back()
     {
+        if (pauseManager.CurrentState != PauseState.Settings) return;
+
         Close();
         pauseManager.PopState();
     }
